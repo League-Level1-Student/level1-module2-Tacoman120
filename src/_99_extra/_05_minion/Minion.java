fe06b08d0b4b07027529fda83203a2ada2642f2b
league <league@imac-24.attlocal.net>
2023-08-11 16:33:44 -0700

    Minion

--- src/_99_extra/_05_minion/Minion.java
+++ src/_99_extra/_05_minion/Minion.java
@@ -1,7 +1,8 @@
 package _99_extra._05_minion;
 
 public class Minion{
-	private String name; 
+	
+		private String name; 
 		private int eyes; 
 		private String color; 
 		private String master;
@@ -9,8 +10,27 @@ public class Minion{
 		public Minion(String name, int eyes, String color, String master) {
 			this.name = name;
 			this.eyes = eyes;
-			 public String getName() {
-				 return;
-			 }
+			this.color = color;
+			this.master = master;
+			
+		
+			 
+		
+}
+		public String getName() {
+			return name;
 		}
+		public int getEyes() {
+			return eyes;	
+		}
+		public String getColor() {
+			return color;
+		}
+		public String getMaster() {
+			return master;
+		}
+		public void setMaster(String master) {
+			this.master = master;
+		}
+	
 }
